fe695debb1abdeb95c88c2d9b1cdae391a34691b
Omansh Awasthi <omanshawasthi@gmail.com>
2026-01-03 12:27:35 +0530

    Implement invoice routing and controller management

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
  import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
+import { connectDB } from './config/db.js';
+import path from 'path';
 
 // ⭐ ADD CLERK MIDDLEWARE
 import { clerkMiddleware } from "@clerk/express";
+import businessProfileRouter from './routes/businessProfileRouter.js';
+import invoiceRouter from './routes/invoiceRouter.js';
+import aiInvoiceRouter from './routes/aiInvoiceRouter.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -16,6 +21,19 @@ app.use(cors({
 
 //Clerk middleware globally (does NOT protect routes)
 app.use(clerkMiddleware());
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
+
+// Database Connection
+connectDB();
+
+// Static uploads folder
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
+// Routes (unchanged)
+app.use("/api/businessProfile", businessProfileRouter);
+app.use("/api/invoice", invoiceRouter);
+app.use("/api/ai", aiInvoiceRouter);
 
 // Test route
 app.get('/', (req, res) => {
